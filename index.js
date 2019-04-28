ee4b329ecb5afbd75d2f4a2f4efbaa069a1f71ab
EdwardGoomba <ask@edwardvdesign.com>
2019-04-28 15:46:35 -0400

    Refactor reducers and combine

--- index.js
+++ index.js
@@ -1,51 +1,4 @@
-{
-  type: 'ADD_TODO',
-    todo: {
-    id: 0,
-      name: 'Learn Redux',
-        complete: false
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-    id: 0
-}
-
-{
-  type: 'TOGGLE_TODO',
-    id: 0
-}
-
-{
-  type: 'ADD_GOAL',
-    goal: {
-    id: 0,
-      name: 'Run a marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-    id: 0,
-}
-
-// Reducer function
-function todos(state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat([action.todo])
-    case 'REMOVE_TODO':
-      return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO':
-      return state.map((todo) => todo.id !== action.id ? todo :
-        Object.assign({}, todo, { complete: !todo.complete })
-      )
-    default:
-      return state
-  }
-}
-
+// library code ex redux
 function createStore(reducer) {
   // The store should have four parts:
   // 1. The state
@@ -87,14 +40,57 @@ function createStore(reducer) {
   }
 }
 
-const store = createStore()
+// app code
+// setup variables for strings
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+// reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete })
+      )
+    default:
+      return state
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal])
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app(state = {}, action) {
+  return ({
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  })
+}
+
+const store = createStore(app)
 
 store.subscribe(() => {
   // pass in function that will be called whenever internal state / store changes
 })
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
     name: 'Learn Redux',
